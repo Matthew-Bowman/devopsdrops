65546510837537df6aa0f207b1e27c75db2eb8fa
Matthew Bowman <matthew.bowman@tutanota.com>
2026-07-14 19:03:18 +0100

    Installed prismjs npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-//
+import Prism from 'prismjs';
+
+// Theme
+import 'prismjs/themes/prism-tomorrow.css';
+
+// Languages
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-yaml';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-markup'; // HTML
+import 'prismjs/components/prism-sql';
+
+Prism.highlightAll();//
